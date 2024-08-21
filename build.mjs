24618266137e9eae5c1360e0235d658af025ffac
Sebastian Jara <sebastian@accountflow.no>
2024-08-21 08:11:28 +0200

    Deps, improved build

--- build.mjs
+++ build.mjs
@@ -1,8 +1,18 @@
 import dts from 'bun-plugin-dts';
 
-await Bun.build({
+const result = await Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   minify: true,
-  plugins: [dts()],
+  target: 'bun',
+  // plugins: [dts()], // This currently doesn't work. Cannot be used in the build script to generate dts files.
 });
+
+if (!result.success) {
+  console.error('Build failed');
+  for (const message of result.logs) {
+    console.error(message);
+  }
+} else {
+  console.info('Build succeeded');
+}
